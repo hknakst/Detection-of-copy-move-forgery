fd665b77aee7eb2e56de02612fb9f457b68131a8
Hakan Aksüt <hakan.aksut58@gmail.com>
2019-04-20 03:25:22 +0300

    ciddi performans artisi saglandi

--- DetectionOfCopyMoveForgery.py
+++ DetectionOfCopyMoveForgery.py
@@ -18,7 +18,7 @@ class DetectionofCopyMoveForgery:
     def detection_forgery(self):
         self.dct_of_img()
         self.lexicographically_sort_of_vectors(self.vector,self.sizeof_vector)
-        self.correlation_of_vectors(0.1 ,5)
+        self.correlation_of_vectors(30,30)
         cv2.imshow("sonuc",self.img)
         cv2.waitKey(0)
 
@@ -28,9 +28,13 @@ class DetectionofCopyMoveForgery:
         for r in range(0, self.height - self.blocksize_r, self.blocksize_r):
             for c in range(0, self.width - self.blocksize_c, self.blocksize_c):
                 block = self.img[r:r + self.blocksize_r, c:c + self.blocksize_c]
-                imf = np.float32(block) / 255.0  #0 ile 1 arasında normalize ediyoruz
-                dst = np.round(cv2.dct(imf),6)      # block block dst uyguluyoruz ve virgülden sonraki 6. basamağı yumarlıyoruz
-                self.significant_part_extraction(self.zigzag(dst),self.sizeof_vector,c,r)
+                # imf = np.float32(block) / 255.0  #0 ile 1 arasında normalize ediyoruz
+                # dst = np.round(cv2.dct(imf),6)      # block block dst uyguluyoruz ve virgülden sonraki 6. basamağı yumarlıyoruz
+                img2 = np.zeros((self.blocksize_r, self.blocksize_c), dtype=np.float32)
+                img2 = img2 + block[:self.blocksize_r, :self.blocksize_c]
+                dct = cv2.dct(img2)
+
+                self.significant_part_extraction(self.zigzag(dct),self.sizeof_vector,c,r)
 
 
 
@@ -78,28 +82,40 @@ class DetectionofCopyMoveForgery:
         for i in range((self.blocksize_c * self.blocksize_r),significant_size, -1):
             vector.pop()
 
+
         vector.append(x)  # blogun baslangıc kordinatının x noktası vektorun sonuna ekleniyor
         vector.append(y)  # aynı sekilde y noktası ekleniyor
         # her block sonucunu vector listemize atiyoruz.
         self.vector.append(vector)
 
-    def lexicographically_sort_of_vectors(self,vector,sort_size):
 
-        for i in range(sort_size):
-            for j in range(len(vector)):
-                for k in range(len(vector) - 1):
-                    if (i == 0):
-                        if (vector[k + 1][i] < vector[k][i]):
-                            temp = vector[k]
-                            vector[k] = vector[k + 1]
-                            vector[k + 1] = temp
+    def lexicographically_sort_of_vectors(self,vector,size):
+        #sutun siralaması oldugu icin ters cevirip siralamamiz lazim
+        #ayni zamanda son iki yani ters cevirince ilk iki satirida koordinat bilgimiz var bunları sıralamicaz.
+        asd=np.array(vector)
+        vector= asd[np.lexsort(np.rot90(asd)[2:(size+1)+2 ,:])]
 
-                    else:
-                        if (vector[k + 1][i - 1] == vector[k][i - 1]):
-                            if (vector[k + 1][i] < vector[k][i]):
-                                temp = vector[k]
-                                vector[k] = vector[k + 1]
-                                vector[k + 1] = temp
+
+        # for i in range(sort_size):
+        #     for j in range(len(vector)):
+        #         for k in range(len(vector) - 1):
+        #             if (i == 0):
+        #                 if (vector[k + 1][i] < vector[k][i]):
+        #                     temp = vector[k]
+        #                     vector[k] = vector[k + 1]
+        #                     vector[k + 1] = temp
+        #
+        #             else:
+        #                 sum =0
+        #                 for h in range(1,i+1):
+        #                     if (vector[k + 1][i - h] == vector[k][i - h]):
+        #                         sum +=1
+        #
+        #                 if(sum==i):
+        #                     if (vector[k + 1][i] < vector[k][i]):
+        #                         temp = vector[k]
+        #                         vector[k] = vector[k + 1]
+        #                         vector[k + 1] = temp
 
 
 
@@ -116,12 +132,11 @@ class DetectionofCopyMoveForgery:
                     v1.append(self.vector[i][-1]) #y1
                     v2.append(self.vector[j][-2]) #x2
                     v2.append(self.vector[j][-1]) #y2
-                    self.elimination_of_weak_vectors(v1,v2,2,10)
+                    self.elimination_of_weak_vectors(v1,v2,2,20)
 
 
     def elimination_of_weak_vectors(self,vector1,vector2,size,threshold_lenght):
         if(self.oklid(vector1,vector2,size) >= threshold_lenght):
-            print(self.oklid(vector1,vector2,size))
             cv2.line(self.img,(vector1[0],vector1[1]),(vector2[0],vector2[1]),(0),2)
             self.elimination_of_weak_area(vector1,vector2,10)
 
